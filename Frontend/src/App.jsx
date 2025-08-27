a8988d9854c5f0ddaac62603c2d0d8bd276aa217
Saurabh209 <saurabhhh2613@gmail.com>
2025-08-28 03:57:27 +0530

    data fetched from backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 
-import { Routes, Route,Link } from 'react-router-dom'
- 
+import { Routes, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+
 
 
 import './index.css'
@@ -10,7 +11,7 @@ import { Sword, Crown, Shield, BookOpen, MapPin, Users, Skull, Star, Hammer, Wan
 import Bosses from './components/boss/Bosses'
 
 import AllDemigods from './components/demigods/AllDemigods';
- 
+
 
 import HomePage from './Home';
 import EpicBattles from './components/epicBattles/EpicBattles';
@@ -21,17 +22,34 @@ import Exploration from './components/Exploration/Exploration';
 
 
 
+
+
 function App() {
+
+
+  const [knowledge, setKnowledge] = useState([]);
+
+  useEffect(() => {
+    axios.get("/lore/knowledge")
+      .then((response) => {
+        setKnowledge(response.data);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },[])
+  console.log(knowledge);
   return (
     <>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/AllDemigods" element={<AllDemigods />} />
         <Route path="/Alldemigods/:bossId" element={<SingleDemigod />} />
         <Route path="/bosses" element={<Bosses />} />
         <Route path="/battles" element={<EpicBattles />} />
-        <Route path='/Lore' element={<Lore/>} /> 
-        <Route path='/exploration' element={<Exploration/>}/>
+        <Route path='/Lore' element={<Lore />} />
+        <Route path='/exploration' element={<Exploration />} />
       </Routes>
 
     </>
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/lore': 'http://localhost:3000'
+    }
+  },
   plugins: [ tailwindcss(),react()],
 })
